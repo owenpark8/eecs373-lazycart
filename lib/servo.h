e7bd0cd31af9e189a4b83aa63ea09bb7c1bc8215
owenpark <owenpark@umich.edu>
2024-04-04 22:07:58 -0400

    Add servo interface and symlink

---
+++ lib/servo.h
@@ -0,0 +1,25 @@
+#ifndef servo_h
+#define servo_h
+
+#include "main.h"
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct Servo {
+    TIM_HandleTypeDef* pwm_tim;
+    uint32_t ccr;
+} Servo;
+
+Servo* servo_ctor() {
+    Servo* servo = (Servo*)malloc(sizeof(Servo));
+    if (!servo) Error_Handler();
+    return servo;
+}
+void servo_dtor(Servo* servo) {
+    free(servo);
+}
+
+// __HAL_TIM_SET_CAPTUREPOLARITY()
+
+
+#endif
